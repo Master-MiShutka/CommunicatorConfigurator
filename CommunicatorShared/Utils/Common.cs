089df330600d6e2375a1c9d9757d0e7c2da90fad
Master-MiShutka <trus.mikhail@tut.by>
2025-08-04 21:57:52 +0300

    Remove duplicate code

--- CommunicatorShared/Utils/Common.cs
+++ CommunicatorShared/Utils/Common.cs
@@ -2,9 +2,7 @@ namespace TMP.Work.CommunicatorPSDTU.Common.Utils;
 
 using System;
 using System.ComponentModel;
-using System.Diagnostics;
 using System.Reflection;
-using System.Text;
 
 /// <summary>
 /// https://stackoverflow.com/questions/479410/enum-tostring-with-user-friendly-strings
@@ -36,94 +34,5 @@ public static class Common
 
         //If we have no description attribute, just return the ToString of the enum
         return enumerationValue.ToString() ?? "???";
-    }
-
-    public static string GetExceptionDetails(Exception? exp)
-    {
-        string message = string.Empty;
-
-        if (exp == null)
-        {
-            return message;
-        }
-
-        if (exp is AggregateException)
-        {
-            if (exp is not AggregateException ae)
-            {
-                return message;
-            }
-
-            foreach (var e in ae.InnerExceptions)
-            {
-                message += Environment.NewLine + e.Message + Environment.NewLine;
-            }
-
-            ParseException(ae.InnerExceptions.LastOrDefault());
-        }
-        else
-        {
-            ParseException(exp);
-        }
-
-        message += "\n" + BuildStackTrace(exp);
-
-        void ParseException(Exception? e)
-        {
-            if (e == null)
-            {
-                return;
-            }
-
-            try
-            {
-                // Write Message tree of inner exception into textual representation
-                message = e.Message;
-
-                if (e.InnerException == null)
-                {
-                    return;
-                }
-
-                Exception? innerEx = e.InnerException;
-
-                for (int i = 0; innerEx != null; i++, innerEx = innerEx.InnerException)
-                {
-                    string spaces = string.Empty;
-
-                    for (int j = 0; j < i; j++)
-                    {
-                        spaces += "  ";
-                    }
-
-                    message += "\n" + spaces + "└─>" + innerEx.Message;
-                }
-            }
-            catch
-            {
-            }
-        }
-
-        return message;
-    }
-
-    public static string BuildStackTrace(Exception e)
-    {
-        var st = new StackTrace(e, true);
-        var frames = st.GetFrames();
-        var traceString = new StringBuilder();
-
-        foreach (var frame in frames)
-        {
-            if (frame.GetFileLineNumber() < 0)
-            {
-                continue;
-            }
-
-            var str = $"file: {frame.GetFileName()}, method:{frame.GetMethod()?.Name ?? "<?>"},  lineNumber:{frame.GetFileLineNumber()};\n";
-            traceString = traceString.Append(str);
-        }
-
-        return traceString.ToString();
     }
 }
