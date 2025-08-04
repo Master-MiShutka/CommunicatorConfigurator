8a4cf5301e3e72e3574360cd651a12dd3f5941a8
Master-MiShutka <trus.mikhail@tut.by>
2025-08-04 21:59:17 +0300

    Fix logic when tabitem selected

--- WpfApp/MainWindow.xaml.cs
+++ WpfApp/MainWindow.xaml.cs
@@ -29,18 +29,24 @@ namespace TMP.Work.CommunicatorPSDTU.UI.Wpf
         {
             if (sender is TabControl tabControl && tabControl.Name == "MainTabs" && e.OriginalSource is TabControl)
             {
-                if (tabControl.SelectedIndex != 1)
+                string? tag = (tabControl.SelectedItem as TabItem)?.Tag?.ToString();
+
+                if (string.IsNullOrEmpty(tag))
                 {
                     if (this.WindowState == WindowState.Maximized)
                     {
                         this.WindowState = WindowState.Normal;
                     }
 
-                    this.Width = this.oldWidth;
-                    this.Height = this.oldHeight;
+                    if (this.oldWidth != 0 && this.oldHeight != 0)
+                    {
+                        this.Width = this.oldWidth;
+                        this.Height = this.oldHeight;
+                    }
                 }
-                if (tabControl.SelectedIndex == 1)
+                else
                 {
+                    // selected CheckContent tab
                     if (this.WindowState == WindowState.Maximized)
                     {
                         this.oldWidth = this.MinWidth;
@@ -48,11 +54,34 @@ namespace TMP.Work.CommunicatorPSDTU.UI.Wpf
                     }
                     else
                     {
-                        this.oldWidth = this.Width == 0 ? this.ActualWidth : this.Width;
-                        this.oldHeight = this.Height == 0 ? this.ActualHeight : this.Height;
+                        if (tag == "CheckContent")
+                        {
+                            this.oldWidth = this.Width == 0 ? this.ActualWidth : this.Width;
+                            this.oldHeight = this.Height == 0 ? this.ActualHeight : this.Height;
 
-                        this.Width += 600d;
-                        this.Height += 200d;
+                            this.Width += 610d;
+                            this.Height += 200d;
+
+                            if (this.Top + this.Height > System.Windows.SystemParameters.VirtualScreenHeight)
+                            {
+                                this.Top -= System.Windows.SystemParameters.VirtualScreenHeight - this.Height;
+                            }
+
+                            if (this.Left + this.Width > System.Windows.SystemParameters.VirtualScreenWidth)
+                            {
+                                this.Left -= System.Windows.SystemParameters.VirtualScreenWidth - this.Width;
+                            }
+
+                            if (this.Left < 0)
+                            {
+                                this.Left = 0;
+                            }
+
+                            if (this.Top < 0)
+                            {
+                                this.Top = 0;
+                            }
+                        }
                     }
                 }
             }
